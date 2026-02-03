d091b11f7a4a4015196b67cf636e317aa9872f97
Kimmo Ahola @ Systementor <kimmo.ahola@systementor.se>
2026-02-03 13:58:18 +0100

    Update cashier_routes.py

--- routes/cashier_routes.py
+++ routes/cashier_routes.py
@@ -6,7 +6,7 @@ from models.model import UserRoles
 cashier_bp = Blueprint("cashier", __name__, url_prefix="/cashier")
 
 @cashier_bp.route("/")
-@auth_required()
-@roles_required(UserRoles.CASHIER.value)
+@auth_required() # use @auth_required() or @login_required to enforce login status. Remember to put this decorator on all routes you wish to protect.
+@roles_required(UserRoles.CASHIER.value) # adds role requirement to the route. We need to be logged in AND have the correlt role(s)
 def start():
     return render_template("cashier.html")
